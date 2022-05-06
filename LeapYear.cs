29182eeb8f9b91aa2a27f7387c710a276166052f
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 17:47:19 +0530

    Program for checking Leap year is added

---
+++ LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearProgram
+{
+    internal class LeapYear
+    {
+        public void checkLeapYearOrNot()
+        {
+            Console.WriteLine("Enter The Year ");
+            int year =Convert.ToInt32(Console.ReadLine());
+            if(year >=1000 && year <= 9999)
+            {
+                if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                {
+                    Console.WriteLine("Leap year");
+                }
+                else
+                {
+                    Console.WriteLine("Not a leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid Year");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeapYearProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to leap year program");
+            LeapYear myobj=new LeapYear();
+            myobj.checkLeapYearOrNot();
+        }
+    }
+}
